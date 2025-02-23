aec44eb737ee29345f13585b6f8845b789f747ea
Mayank Gupta <mayank.guptacse11@gmail..com>
2025-02-23 19:27:10 +0530

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components';
-import useDarkMode from 'use-dark-mode';
-import AppContext from './AppContext';
-import MainApp from './MainApp';
-import GlobalStyles from './theme/GlobalStyles';
-import { lightTheme, darkTheme } from './theme/themes';
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import useDarkMode from "use-dark-mode";
+import AppContext from "./AppContext";
+import MainApp from "./MainApp";
+import GlobalStyles from "./theme/GlobalStyles";
+import { lightTheme, darkTheme } from "./theme/themes";
+import Chatbot from "./components/Chatbot"; // Import Chatbot
 
 function App() {
   window.matchMedia = null;
@@ -20,6 +21,7 @@ function App() {
         <div className="App">
           <BrowserRouter>
             <MainApp />
+            <Chatbot /> {/* Add chatbot here */}
           </BrowserRouter>
         </div>
       </ThemeProvider>
